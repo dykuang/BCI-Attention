f3418dfecc6bf3b1331ab7f9fb2c2b4c47e5e928
Dongyang Kuang <dykuangii@gmail.com>
2022-07-27 09:48:32 +0800

    Update cv_exp.sh

--- cv_exp.sh
+++ cv_exp.sh
@@ -1,6 +1,6 @@
 for i in {1..15}
 do
   echo "CV benchmark on subject: $i, start ..."
-  CUDA_VISIBLE_DEVICES=1 python exp_5CV_KAN.py --subject $i
+  CUDA_VISIBLE_DEVICES=1 python exp_5CV_SEED.py --subject $i
   echo "CV benchmark on subject: $i, finished"
 done
